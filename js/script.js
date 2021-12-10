cdbe4d89449b5e6621dcabf9d49d34fe3a11e493
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2021-12-10 15:23:42 +0100

    Button reset to default state

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Button 'Расчитать' becomes active only when all input fields are filled.
+// Button "Расчитать" becomes active only when all input fields are filled.
 const btnSubmit = document.querySelector(".form__submit-button");
 const ageInput = document.getElementById("age");
 const heightInput = document.getElementById("height");
@@ -29,4 +29,26 @@ form.addEventListener("submit", function (evt) {
   if (result.classList.contains("counter__result--hidden")) {
     result.classList.remove("counter__result--hidden");
   }
+});
+
+// Button "Очистить поля и расчёт" becomes active when at least one numeric field is filled.
+// All elements of the application are reset to their default state
+const btnReset = document.querySelector(".form__reset-button");
+
+inputs.forEach(function (input) {
+  input.addEventListener("change", function () {
+    const age = Number(ageInput.value);
+    const height = Number(heightInput.value);
+    const weight = Number(weightInput.value);
+
+    if (age || height || weight >= 1) {
+      btnReset.removeAttribute("disabled");
+    }
+  });
+});
+
+form.addEventListener("reset", function () {
+  btnReset.setAttribute("disabled", "disabled");
+  btnSubmit.setAttribute("disabled", "disabled");
+  result.classList.add("counter__result--hidden");
 });
