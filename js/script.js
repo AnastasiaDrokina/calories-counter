07aa7a66dd40b7d685d3ac55239f217a288d71f3
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2021-12-15 12:14:43 +0100

    Add weight gain and loss formulas

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ const form = document.querySelector(".counter__form");
 const btnSubmit = document.querySelector(".form__submit-button");
 const btnReset = document.querySelector(".form__reset-button");
 const result = document.querySelector(".counter__result");
+const male = document.getElementById("gender-male");
+const female = document.getElementById("gender-female");
 let caloriesNorm = document.getElementById("calories-norm");
+let caloriesMin = document.getElementById("calories-minimal");
+let caloriesMax = document.getElementById("calories-maximal");
 
 form.addEventListener("change", function (evt) {
   const age = Number(evt.target.form.elements.age.value);
@@ -29,15 +33,30 @@ form.addEventListener("submit", function (evt) {
     result.classList.remove("counter__result--hidden");
   }
 
-  // // Maintaining weight
+  // Maintaining weight
   const age = Number(evt.target.elements.age.value);
   const height = Number(evt.target.elements.height.value);
   const weight = Number(evt.target.elements.weight.value);
+  const percentageNumberMen = (countMen(age, height, weight) / 100) * 15;
 
-  function countWomen(weight, height, age) {
-    return 10 * weight + 6.25 * height - 5 * age - 161;
+  function countMen(weight, height, age) {
+    return 10 * weight + 6.25 * height - 5 * age + 5;
   }
-  caloriesNorm.textContent = countWomen(age, height, weight);
+
+  caloriesNorm.textContent = countMen(age, height, weight);
+
+  caloriesMax.textContent = Math.round(
+    percentageNumberMen + countMen(age, height, weight)
+  );
+
+  caloriesMin.textContent = Math.round(
+    countMen(age, height, weight) - percentageNumberMen
+  );
+
+  // function countWomen(weight, height, age) {
+  //   return 10 * weight + 6.25 * height - 5 * age - 161;
+  // }
+  // caloriesNorm.textContent = countWomen(age, height, weight);
 });
 
 // All elements of the application are reset to their default state
